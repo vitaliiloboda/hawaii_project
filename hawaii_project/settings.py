f63c106eb37c2a97ea29c4512002994cf54df531
Turganov Ivan <mrdurandall@gmail.com>
2022-08-28 11:36:37 +0300

    project configured for django_channels

--- hawaii_project/settings.py
+++ hawaii_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Channels
+ASGI_APPLICATION = "hawaii_project.asgi.application"
