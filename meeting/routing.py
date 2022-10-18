a8ee31f737be0fc37ac4676e3faa4f2d636394a0
Turganov Ivan <mrdurandall@gmail.com>
2022-10-18 13:36:28 +0300

    created blank websocket for camera

--- meeting/routing.py
+++ meeting/routing.py
@@ -4,7 +4,7 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/meeting/camera/(?P<meeting_id>\w+)/$', consumers.VideoConsumer.as_asgi()),
-    re_path(r'ws/meeting/projector/(?P<meeting_id>\w+)/$', consumers.VideoConsumer.as_asgi()),
-    re_path(r'ws/meeting/distant/(?P<meeting_id>\w+)/$', consumers.VideoConsumer.as_asgi()),
+    re_path(r'ws/meeting/camera/(?P<meeting_id>\w+)/$', consumers.CameraConsumer.as_asgi()),
+    re_path(r'ws/meeting/projector/(?P<meeting_id>\w+)/$', consumers.ProjectorConsumer.as_asgi()),
+    re_path(r'ws/meeting/distant/(?P<meeting_id>\w+)/$', consumers.DistantConsumer.as_asgi()),
 ]
