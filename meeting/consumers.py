a2634ead63e246747d20e519028550434aabb73a
Turganov Ivan <mrdurandall@gmail.com>
2022-10-19 11:35:06 +0300

    created blank wensockets for projector and distant user

--- meeting/consumers.py
+++ meeting/consumers.py
@@ -31,11 +31,38 @@ class ProjectorConsumer(WebsocketConsumer):
     """ Консьюмер, получающий данные от пользователя, запустившего приложение в режиме проектора
 
     """
-    pass
+    def connect(self):
+        self.meeting_id = self.scope['url_routes']['kwargs']['meeting_id']
+        self.group_name = f'camera_{self.meeting_id}'
+
+        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+
+        self.accept
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.group_name, self.channel_name)
+
+    def receive(self, text_data=None, bytes_data=None):
+        pass
 
 
 class DistantConsumer(WebsocketConsumer):
     """ Консьюмер, получающий данные от пользователя, запустившего приложение в удаленном режиме
 
     """
-    pass
+
+    def connect(self):
+        self.meeting_id = self.scope['url_routes']['kwargs']['meeting_id']
+        self.camera_group_name = f'camera_{self.meeting_id}'
+        self.projector_group_name = f'projector_{self.meeting_id}'
+        self.distant_group_name = f'distant_{self.meeting_id}'
+
+        async_to_sync(self.channel_layer.group_add)(self.camera_group_name, self.channel_name)
+
+        self.accept
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.camera_group_name, self.channel_name)
+
+    def receive(self, text_data=None, bytes_data=None):
+        pass
