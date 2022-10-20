2ede90ac70a04c196ffc3ce325bb7c79a7c16eeb
Turganov Ivan <mrdurandall@gmail.com>
2022-10-20 11:46:53 +0300

    resolved mn-53

--- hawaii_project/settings.py
+++ hawaii_project/settings.py
@@ -147,7 +147,9 @@ SIMPLE_JWT = {
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 
 # настройки rest framework
