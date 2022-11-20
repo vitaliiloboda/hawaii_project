3d5570bf2d63cb8a7b3bb170cf4f85cf86bfb49d
Turganov Ivan <mrdurandall@gmail.com>
2022-11-20 19:59:36 +0300

    added users field in meeting model. deleted UsersInMeetings model

--- meeting/models.py
+++ meeting/models.py
@@ -4,11 +4,35 @@ from datetime import datetime
 
 
 class Meeting(models.Model):
-    name = models.CharField(max_length=200, verbose_name='meeting name', default='random_meeting')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='meeting owner')
-    start_time = models.DateTimeField(auto_now_add=True, verbose_name='meeting start time')
-    end_time = models.DateTimeField(blank=True, null=True)
-    password = models.CharField(max_length=50, verbose_name='meeting password', default='mypassword')
+    name = models.CharField(
+        max_length=200,
+        verbose_name='meeting name',
+        default='random_meeting'
+    )
+    owner = models.ForeignKey(
+        User,
+        related_name='owned_meetings',
+        on_delete=models.CASCADE,
+        verbose_name='meeting owner'
+    )
+    start_time = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='meeting start time'
+    )
+    end_time = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name='meeting end time'
+    )
+    password = models.CharField(
+        max_length=50,
+        verbose_name='meeting password',
+        default='mypassword'
+    )
+    users = models.ManyToManyField(
+        User,
+        related_name='meetings'
+    )
 
     def __str__(self):
         return self.name
@@ -33,38 +57,6 @@ class MeetingImages(models.Model):
 
     def __str__(self):
         return f'{self.meeting.name} image {self.id}'
-
-
-class UsersInMeeting(models.Model):
-    PROJECTOR = 0
-    CAMERA = 1
-    GUEST = 2
-
-    NOTE = (
-        (PROJECTOR, 'projector'),
-        (CAMERA, 'camera'),
-        (GUEST, 'guest'),
-    )
-
-    role = models.IntegerField(
-        choices=NOTE,
-        verbose_name='role'
-    )
-    meeting = models.ForeignKey(
-        Meeting,
-        related_name='users',
-        related_query_name='user',
-        on_delete=models.CASCADE
-    )
-    user = models.ForeignKey(
-        User,
-        related_name='meetings',
-        related_query_name='meeting',
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return f'{self.meeting.name} users'
 
 
 class Layer(models.Model):
